332dd91eb3458015af8dd6406f162223e92b3730
Alvaro <alvaro@asanera.com>
2022-12-06 21:45:08 +0100

    Import pokemonAPI to the test unit

--- tests/unit/pokemonApi.spec.js
+++ tests/unit/pokemonApi.spec.js
@@ -1,3 +1,7 @@
+import pokemonApi from "@/api/pokemonApi"
+
 describe('pokemomApi', () => {
-    test('axio debe estar bien configurado')
+    test('axio debe estar bien configurado', () => {
+        console.log(pokemonApi);
+    })
 })
