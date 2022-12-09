34e0ba42be15a4c681fe14cb2d5faac4e3d6052e
Alvaro <alvaro@asanera.com>
2022-12-09 12:18:05 +0100

    Test component PokemonOption

--- tests/unit/component/PokemonOption.spec.js
+++ tests/unit/component/PokemonOption.spec.js
@@ -1,6 +1,6 @@
-import PokemonOption from "@/components/PokemonOption";
-import { shallowMount } from "@vue/test-utils";
-import { pokemons } from "../mocks/pokemons.mock";
+import PokemonOption from "@/components/PokemonOption"
+import { shallowMount } from "@vue/test-utils"
+import { pokemonsMock } from "../mocks/pokemons.mock"
 
 describe('Test PokemonOption', () => {
 
@@ -8,28 +8,30 @@ describe('Test PokemonOption', () => {
     beforeAll(() => {
          wrapper = shallowMount(PokemonOption, {
             props: {
-                pokemons
+                pokemons: pokemonsMock
             }
-         });
-    });
+         })
+    })
 
     test('Debe hacer match con el snapshot', () => {
-        expect(wrapper.element).toMatchSnapshot();
+        expect(wrapper.element).toMatchSnapshot()
     })
 
     test('Debe mostrar las cuatro opciones correctamente', () => {
-        wrapper = shallowMount(PokemonOption)
-        const [pokemonOptionA, pokemonOptionB, pokemonOptionC, pokemonOptionD] = wrapper.findAll("li")
+        const [pokemonOptionA, pokemonOptionB, pokemonOptionC, pokemonOptionD] =  wrapper.findAll("li")
         
-        expect(pokemonOptionA).toBe("");
-        expect(pokemonOptionB).toBe("");
-        expect(pokemonOptionC).toBe("");
-        expect(pokemonOptionD).toBe("");
-
-
+        expect(pokemonOptionA.text()).toEqual(pokemonsMock[0].name)
+        expect(pokemonOptionB.text()).toEqual(pokemonsMock[1].name)
+        expect(pokemonOptionC.text()).toEqual(pokemonsMock[2].name)
+        expect(pokemonOptionD.text()).toEqual(pokemonsMock[3].name)
     })
 
-    test('Debe emetir "selection" con sus respectivo parametros', () => {
+    test('Debe emetir "selection" con sus respectivo parametros',  () => {
+        const [pokemonOptionA] =  wrapper.findAll("li")
 
+        pokemonOptionA.trigger('click');
+
+        expect(wrapper.emitted('selectionPokemon').length).toEqual(1);
+        expect(wrapper.emitted('selectionPokemon')[0]).toEqual([5]);
     })
 })
