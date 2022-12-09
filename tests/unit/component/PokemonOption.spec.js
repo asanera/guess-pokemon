082d6c1f81ca4c37cefb28333cc2ddf6df053c99
Alvaro <alvaro@asanera.com>
2022-12-09 12:04:46 +0100

    Test component PokemonOption: Create mock and snapshot test

--- tests/unit/component/PokemonOption.spec.js
+++ tests/unit/component/PokemonOption.spec.js
@@ -1,9 +1,19 @@
 import PokemonOption from "@/components/PokemonOption";
 import { shallowMount } from "@vue/test-utils";
+import { pokemons } from "../mocks/pokemons.mock";
 
 describe('Test PokemonOption', () => {
+
+    let wrapper
+    beforeAll(() => {
+         wrapper = shallowMount(PokemonOption, {
+            props: {
+                pokemons
+            }
+         });
+    });
+
     test('Debe hacer match con el snapshot', () => {
-        const wrapper = shallowMount(PokemonOption);
-        expect(wrapper).toMatchSnapshot();
+        expect(wrapper.element).toMatchSnapshot();
     })
 })
---
+++ tests/unit/mocks/pokemons.mock.js
@@ -0,0 +1,6 @@
+export const pokemons = [
+    {name: 'pikachu', id: 5},
+    {name: 'ivysaur', id: 10},
+    {name: 'venusaur', id: 2},
+    {name: 'charmander', id: 20}
+]
