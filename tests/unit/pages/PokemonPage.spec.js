3e8fd5b7eb2eab36d39b5c08098d251b7f39e00d
Alvaro <alvaro@asanera.com>
2022-12-09 14:14:04 +0100

    Test component PokemonPage

--- tests/unit/pages/PokemonPage.spec.js
+++ tests/unit/pages/PokemonPage.spec.js
@@ -1,25 +1,27 @@
 import PokemonPage from '@/pages/PokemonPage'
-import { mount, shallowMount } from '@vue/test-utils'
+import { shallowMount } from '@vue/test-utils'
 import { pokemonsMock } from '../mocks/pokemons.mock'
 
 describe('PokemonPage Component', () => {
 
     let wrapper
-    beforeAll(() => {
-       wrapper =  shallowMount(PokemonPage, {
+    let newGameSyp
+    beforeEach(() => {
+        newGameSyp = jest.spyOn(PokemonPage.methods, 'newGame')
+        wrapper = shallowMount(PokemonPage, {
             data() {
                 return {
                     pokemon: pokemonsMock,
                     pokemonPropouse: pokemonsMock[0],
                     showPokemon: false,
                     showAnswer: false,
-                    message: ''                    
+                    message: ''
                 }
             }
         })
     })
 
-    test('Debe de hacer match con el snapshot', () =>{
+    test('Debe de hacer match con el snapshot', () => {
         expect(wrapper.html()).toMatchSnapshot()
     })
 
@@ -33,7 +35,7 @@ describe('PokemonPage Component', () => {
         const pokemonPicture = wrapper.find('pokemon-picture-stub')
         expect(pokemonPicture.exists).toBeTruthy()
         expect(pokemonPicture.attributes('pokemonid')).toEqual('5')
-        
+
         const pokemonOption = wrapper.find('pokemon-option-stub')
         expect(pokemonOption.exists).toBeTruthy()
         expect(pokemonOption.attributes('pokemons')).not.toBeNull()
@@ -41,19 +43,15 @@ describe('PokemonPage Component', () => {
     })
 
     test('Debe llamar newGame', async () => {
-        const newGameSyp = jest.spyOn(PokemonPage.methods, 'newGame')
-        wrapper =  shallowMount(PokemonPage, {
-            data() {
-                return {
-                    pokemon: pokemonsMock,
-                    pokemonPropouse: pokemonsMock[0],
-                    showPokemon: false,
-                    showAnswer: true,
-                    message: ''                    
-                }
-            }
-        })        
+        await wrapper.setData({ showAnswer: true })
         wrapper.find('button').trigger('click')
         expect(newGameSyp).toHaveBeenCalled()
+    })
+
+    test('Debe checkear la respuesta', async () => {
+        await wrapper.vm.checkAnswer(5)
+        expect(wrapper.find('h2').exists).toBeTruthy()
+        expect(wrapper.vm.showAnswer).toBeTruthy()
+
     })
 })
