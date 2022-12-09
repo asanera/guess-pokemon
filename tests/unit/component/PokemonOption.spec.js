bf8e3bb9544dcf181426d569eeaed3c5f22d3057
Alvaro <alvaro@asanera.com>
2022-12-09 12:16:07 +0100

    Prepare except

--- tests/unit/component/PokemonOption.spec.js
+++ tests/unit/component/PokemonOption.spec.js
@@ -18,10 +18,18 @@ describe('Test PokemonOption', () => {
     })
 
     test('Debe mostrar las cuatro opciones correctamente', () => {
+        wrapper = shallowMount(PokemonOption)
+        const [pokemonOptionA, pokemonOptionB, pokemonOptionC, pokemonOptionD] = wrapper.findAll("li")
+        
+        expect(pokemonOptionA).toBe("");
+        expect(pokemonOptionB).toBe("");
+        expect(pokemonOptionC).toBe("");
+        expect(pokemonOptionD).toBe("");
+
 
     })
 
     test('Debe emetir "selection" con sus respectivo parametros', () => {
-        
+
     })
 })
