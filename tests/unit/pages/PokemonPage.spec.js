ceae0aff726c16a7fecdf3307db193ecb5c238ae
Alvaro <alvaro@asanera.com>
2022-12-09 13:02:36 +0100

    Test component PokemonPage

--- tests/unit/pages/PokemonPage.spec.js
+++ tests/unit/pages/PokemonPage.spec.js
@@ -9,4 +9,10 @@ describe('PokemonPage Component', () => {
     test('Debe de hacer match con el snapshot', () =>{
         expect(wrapper.element).toMatchSnapshot()
     })
+
+    test('Debe llamar mixPokemonArray al montar', () => {
+        const mixPokemonArraySyp = jest.spyOn(PokemonPage.methods, 'mixPokemonArray')
+        mount(PokemonPage);
+        expect(mixPokemonArraySyp).toHaveBeenCalled()
+    })
 })
